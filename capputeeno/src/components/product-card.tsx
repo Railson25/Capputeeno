4a4fb1ca2bdb903873353bb19ba15342700c43ed
Railson25 <railsonsantiago123@gmail.com>
2023-07-14 13:32:26 -0300

    <feat>: Adding Navigation between CardList and CardProduct pages

--- capputeeno/src/components/product-card.tsx
+++ capputeeno/src/components/product-card.tsx
@@ -1,10 +1,12 @@
 import { formatPrice } from "@/utils/format-price"
+import { useRouter } from "next/navigation"
 import { styled } from "styled-components"
 
 interface ProductCardProps{
     image: string,
     title: string,
-    price: number
+    price: number,
+    id: string
 }
 
 const Card = styled.div`
@@ -16,6 +18,7 @@ const Card = styled.div`
     background: rgba(255, 255, 255, 0.4);
     backdrop-filter: blur(10px);
     width: 256px;
+    cursor: pointer;
 
     img{
         width: 256px;
@@ -58,9 +61,14 @@ const Card = styled.div`
 
 export function ProductCard(props : ProductCardProps){
     const price = formatPrice(props.price)
+    const router = useRouter()
+
+    const handleNavigate = () => {
+        router.push("/product?id=" + props.id)
+    }
 
     return(
-        <Card>
+        <Card onClick={handleNavigate}>
             <img src={props.image} />
             <div>
                 <h3>{props.title}</h3>
