92253eefdc2218f404a7c651d627b0c2a23685ee
Railson25 <railsonsantiago123@gmail.com>
2023-07-14 13:20:35 -0300

    <feat>: Adding Product Page

--- capputeeno/src/app/product/page.tsx
+++ capputeeno/src/app/product/page.tsx
@@ -2,6 +2,10 @@
 
 import { BackButton } from "@/components/back-button"
 import { DefaultPageLayout } from "@/components/default-page-layout"
+import { ShoppingIcon } from "@/components/icons/shopping-icon"
+import { useProduct } from "@/hooks/useProduct"
+import { formatPrice } from "@/utils/format-price"
+
 
 import { styled } from "styled-components"
 
@@ -14,16 +18,122 @@ const Container = styled.div`
     align-items: flex-start;
     justify-content: center;
     flex-direction: column;
+
+    section {
+        display: flex;
+        justify-content: center;
+        width: 100%;
+        gap: 32px;
+        margin-top: 24px;
+
+        img{
+            max-width: 640px;
+            width: 50%;
+        }
+
+        > div {
+            display: flex;
+            justify-content: space-between;
+            flex-direction: column;
+            
+            
+            button{
+                border-radius: 4px;
+                background: var(--brand-blue, #115D8C);
+                mix-blend-mode: multiply;
+                border: none;
+                padding: 10px 0;
+                text-align: center;
+                font-size: 16px;
+                font-weight: 500;
+                color: var(--shapes-light);
+                cursor: pointer;
+                font-family: inherit;
+                text-transform: uppercase;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                gap: 12px;
+            }
+        }
+    }
+`
+
+const ProductInfo = styled.div`
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+    flex-direction: column;
+        
+    span {
+        color: var(--text-dark-2);
+        font-family: inherit;
+        font-size: 16px;
+        font-weight: 400;
+        line-height: 150%; 
+        margin-bottom: 24px;
+    }
+    
+    h2{
+        font-size: 32px;
+        font-weight: 300;
+        margin-top: 12px;
+    }
+    span:nth-of-type(2){
+        color: var(--shapes-dark);
+        font-size: 20px;
+        font-weight: 600;
+    }
+
+    p{
+        font-size: 12px;
+        font-weight: 400;
+    }
+
+    div {
+        margin-top: 58px;
+        h3 {
+            text-transform: uppercase;
+            color: var(--text-dark);
+            font-weight: 500;
+            font-size: 16px;
+            margin-bottom: 8px;
+        }
+
+        p{
+        font-size: 14px;
+        }
+    }
 `
 
 
+export default function Product({searchParams}: {searchParams: {id: string}}) {
+    const {data} = useProduct(searchParams.id)
+    
 
-export default function Product(props: ProductProps) {
     return(
         <DefaultPageLayout>
             <Container>
                 <BackButton navigate="/"/>
-                <section>Informações do Produto</section>
+                <section>
+                    <img src={data?.image_url}  />
+                    <div>
+                        <ProductInfo>
+                            <span>{data?.category} </span>
+                            <h2>{data?.name}</h2>
+                            <span>{formatPrice(data?.price_in_cents ?? 0)}</span>
+                            <p>*Frete de R$40,00 para todo o Brasil. Grátis para compras acima de R$900,00.</p>
+                            <div>
+                                <h3>Descrição</h3>
+                                <p>{data?.description}</p>
+                            </div>
+                        </ProductInfo>
+                        <button>
+                            <ShoppingIcon />
+                            Adicionar ao carrinho
+                        </button>
+                    </div>
+                </section>
             </Container>
         </DefaultPageLayout>
     )
---
+++ capputeeno/src/hooks/useProduct.ts
@@ -0,0 +1,34 @@
+
+import { ProductFetchResponse } from "@/types/product";
+import axios, { AxiosPromise } from "axios";
+import { useQuery } from "react-query";
+
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL as string
+
+const fetcher = (productId: string): AxiosPromise<ProductFetchResponse> => {
+    return axios.post(API_URL,{query: `
+    query {
+        Product(id: "${productId}") {
+          id
+          name
+          price_in_cents
+          category
+          sales
+          image_url
+          description
+        }
+      }
+    `})
+}
+
+export function useProduct(id: string){
+    const {data} = useQuery({
+        queryFn: () => fetcher(id),
+        queryKey: ['product', id],
+        enabled: !!id
+    })
+    return {
+        data: data?.data?.data?.Product
+    }
+}
