f4c9b3b2a67135920234e84c35d219ed7502d1e7
Railson25 <railsonsantiago123@gmail.com>
2023-07-17 13:41:11 -0300

    <feat>: Divider component

---
+++ capputeeno/src/components/divider.tsx
@@ -0,0 +1,9 @@
+import { styled } from "styled-components";
+
+export const Divider = styled.span`
+    width: 100%;
+    height: 1px;
+    margin: 8px auto;
+    background: var(--shapes-2);
+    padding: 0;
+`
