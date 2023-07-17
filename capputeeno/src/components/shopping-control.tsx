bc1b119e65a394a1d61ffff8daf778a3a453bebc
Railson25 <railsonsantiago123@gmail.com>
2023-07-17 13:45:55 -0300

    <feat>: Adding navigation to Cart component

--- capputeeno/src/components/shopping-control.tsx
+++ capputeeno/src/components/shopping-control.tsx
@@ -1,6 +1,7 @@
 import { useLocalStorage } from "@/hooks/useLocalStorage";
 import { ShoppingBag } from "./icons/shopping-bag-icon";
 import { styled } from "styled-components";
+import { useRouter } from "next/navigation";
 
 
 const ShoppingCount = styled.span `
@@ -14,15 +15,23 @@ const ShoppingCount = styled.span `
     margin-left: -10px;
 `
 
-const Container = styled.div`
+const Container = styled.button`
     position: relative;
+    border: none;
+    background: transparent;
+    cursor: pointer;
 `
 
 export function ShoppingControl(){
     const {value} = useLocalStorage('shopp-items', [])
+    const router = useRouter()
+
+    const handleNavigateToCart = () => {
+        router.push("/cart")
+    }
 
     return(
-        <Container>
+        <Container onClick={handleNavigateToCart} >
            <ShoppingBag />
             {value.length > 0 && <ShoppingCount>{value.length}</ShoppingCount>}
         </Container>
